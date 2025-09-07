3818c52bd42f1d997ce88000dc1eb84cb13f1c63
ajay muthukumaran <65906852+ajay-mkumar@users.noreply.github.com>
2025-09-07 20:33:17 +0530

    validation schema added

--- src/component/About/WorkDetailsForm.tsx
+++ src/component/About/WorkDetailsForm.tsx
@@ -1,10 +1,11 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
 import FormComponent from "../Form/FormComponent";
 import { Button, DialogActions, DialogContent, TextField } from "@mui/material";
 import type { workDetails } from "../../redux/type/UserType";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { addWorkDetails } from "../../redux/userSlicse";
 import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
 
 interface WorkDetailsFormProps {
   open: boolean;
@@ -16,6 +17,13 @@ function WorkDetailsForm({ open, onClose, workDetails }: WorkDetailsFormProps) {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { username } = useAppSelector((state) => state.user);
+
+  const validationSchema = Yup.object({
+    companyName: Yup.string().required("Company Name is required"),
+    designation: Yup.string().required("Designation is required"),
+    duration: Yup.string().required("Duration is required"),
+    techStack: Yup.string().required("Tech Stack is required"),
+  });
 
   const initialValues = {
     companyName: workDetails?.companyName || "",
@@ -37,38 +45,47 @@ function WorkDetailsForm({ open, onClose, workDetails }: WorkDetailsFormProps) {
     <FormComponent open={open} onClose={onClose} title="Add Work Details">
       <Formik
         initialValues={initialValues}
+        validationSchema={validationSchema}
         onSubmit={(values) => handleSubmit(values)}
       >
-        {({ handleChange, values }) => (
+        {({ handleChange, values, handleBlur }) => (
           <Form>
             <DialogContent dividers>
               <TextField
                 label="name"
                 name="companyName"
                 onChange={handleChange}
+                onBlur={handleBlur}
                 value={values.companyName}
                 sx={{ mb: 2 }}
               />
+              <ErrorMessage component="div" name="companyName" />
               <TextField
                 label="designation"
                 name="designation"
                 onChange={handleChange}
+                onBlur={handleBlur}
                 value={values.designation}
                 sx={{ ml: 2 }}
               />
+              <ErrorMessage component="div" name="designation" />
               <TextField
                 label="duration"
                 name="duration"
                 onChange={handleChange}
+                onBlur={handleBlur}
                 value={values.duration}
               />
+              <ErrorMessage component="div" name="duration" />
               <TextField
                 label="tech Stack"
                 name="techStack"
                 onChange={handleChange}
+                onBlur={handleBlur}
                 value={values.techStack}
                 sx={{ ml: 2 }}
               />
+              <ErrorMessage component="div" name="techStack" />
             </DialogContent>
             <DialogActions>
               <Button onClick={onClose} color="secondary" variant="outlined">
